60ba02d829392030485b6e9d44a3e9b19fcbbc80
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-04-08 18:04:32 +0900

    표준내장객체 - 문자 2
    학습완료

--- javascript/dist/index.63aff760.js
+++ javascript/dist/index.63aff760.js
@@ -1255,8 +1255,41 @@ function hmrAccept(bundle, id) {
 // .padStart()
 // 대상 문자의 길이(length)가 지정된 길이보다 작으면,
 // 주어진 문자를 지정된 길이까지 앞에 붙여 새로운 문자를 반환함.
-const str = "1234567890123";
-console.log(str.padStart(10, "0"));
+// const str = '1234567890123'
+// console.log(str.padStart(10, '0'))
+// console.log(str)
+// .replace()
+// 대상 문자에서 패턴(문자, 정규식)과 일치하는 부분을 교체한 새로운 문자를 반환.
+// const str = 'Hello, Hello?!'
+// console.log(str.replace('Hello','Hi'))
+// console.log(str.replace(/Hello/g, 'Hi'))
+// console.log(str)
+// .slice()
+// 대상 문자의 일부를 추출해 새로운 문자를 반환함.
+// 두 번째 인수 직전까지 추출하고, 두 번쨰 인수를 생략하면 대상 문자의 끝까지 추출합니다. 
+// const str = 'Hello world!'
+// console.log(str.slice(0,5))
+// console.log(str.slice(6, -1))
+// console.log(str.slice(6))
+// console.log(str)
+// .split()
+// 대상 문자를 주어진 구분자로 나눠 배열로 반환함
+// const str = 'Apple / Banana / Cherry'
+// console.log(str.split(' / '))
+// .toLowerCase()
+// 대상 문자를 영어 소문자로 변환해 새로운 문자로 반환함
+// const str = 'Apple, Banana, Cherry'
+// console.log(str.toLocaleLowerCase())
+// console.log(str)
+// .toUpperCase()
+// 대상 문자를 영어 대문자로 변환해 새로운 문자로 반환함
+// const str = 'Apple, Banana, Cherry'
+// console.log(str.toUpperCase())
+// console.log(str)
+// .trim()
+// 대상 문자의 앞뒤 공백 문자(space, tab)등을 제거한 새로운 문자를 반환함
+const str = "   HEROPY!   ";
+console.log(str.trim());
 console.log(str);
 
 },{}]},["3tJLW","adjPd"], "adjPd", "parcelRequirebaba")
