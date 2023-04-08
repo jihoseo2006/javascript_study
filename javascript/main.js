00145308986aa729292a560a7ba99e1f94052adb
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-04-08 20:32:20 +0900

    표준내장객체 - 숫자
    학습완료!!

--- javascript/main.js
+++ javascript/main.js
@@ -970,28 +970,75 @@
 // .trim()
 // 대상 문자의 앞뒤 공백 문자(space, tab)등을 제거한 새로운 문자를 반환함
 
-const str = '   HEROPY!   '
+// const str = '   HEROPY!   '
 
-console.log(str.trim())
-console.log(str)
-
-
+// console.log(str.trim())
+// console.log(str)
 
 
 
+//숫자
+
+// .toFixed()
+// 숫자를 지정된 고정 소수점 표기(자릿수)까지 표현하는 문자로 반환함.
+
+// const num = 3.1415926535
+
+// console.log(num.toFixed(2))
+// console.log(parseFloat(num.toFixed(2))) //parseFloat로 문자 데이터를 숫자로 바꿈
 
 
+// .toLocaleString()
+// 숫자를 현지 언어 방식의 문자로 반환함.
+
+// const num = 1000000
+
+// console.log(num.toLocaleString())
+// console.log(`${num.toLocaleString()}원`)
 
 
+// Number.isInteger()
+// 숫자가 정수인지 확인한다.
+
+// const num = 123
+// const ip = 3.14
+
+// console.log(Number.isInteger(num))
+// console.log(Number.isInteger(ip))
 
 
+// Number.isNaN()
+// 주어진 값이 `NaN` 인지 확인함.
+
+// const num1 = NaN
+// const num2 = 123
+// const str = 'Hello world'
+// const nul = null
+
+// console.log(Number.isNaN(num1))
+// console.log(Number.isNaN(num2))
+// console.log(Number.isNaN(str))
+// console.log(Number.isNaN(nul))
 
 
+// Number.parseInt() 또는 parseInt()
+// 주어진 값(숫자, 문자)을 파싱해 특정 진수(radix)의 정수로 반환함.
+
+// const str = '3.1415926535'
+// const num = 3.1415926535
+
+// console.log(Number.parseInt(str, 10))
+// console.log(parseInt(num, 10)) //함수로도 제공됨!
 
 
+// Number.parseFloat() 또는 parseFloat()
+// 주어진 값(숫자, 문자)을 파싱해 특정 진수(radix)의 부동소수점 실수로 반환함.
 
+const str = '3.1415926535'
+const num = 3.1415926535
 
-
+console.log(Number.parseFloat(str, 10))
+console.log(parseFloat(num, 10)) //함수로도 제공됨!
 
 
 
