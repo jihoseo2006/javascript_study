cda77d48f8320b90bc98f904a1cd5c095573c4c6
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-04-08 21:05:25 +0900

    표준내장객체 - 숫자
    학습완료!

--- javascript/main.js
+++ javascript/main.js
@@ -1034,11 +1034,226 @@
 // Number.parseFloat() 또는 parseFloat()
 // 주어진 값(숫자, 문자)을 파싱해 특정 진수(radix)의 부동소수점 실수로 반환함.
 
-const str = '3.1415926535'
-const num = 3.1415926535
+// const str = '3.1415926535'
+// const num = 3.1415926535
 
-console.log(Number.parseFloat(str, 10))
-console.log(parseFloat(num, 10)) //함수로도 제공됨!
+// console.log(Number.parseFloat(str, 10))
+// console.log(parseFloat(num, 10)) //함수로도 제공됨!
+
+
+//수학
+
+// Math.abs()
+// 주어진 숫자의 절댓값을 반환함.
+
+// console.log(Math.abs(2))
+// console.log(Math.abs(-2))
+
+
+//Math.ceil()
+// 주어진 숫자를 올림해 정수를 반환
+
+// console.log(Math.ceil(3.1415926535))
+
+
+
+//Math.floor()
+// 주어진 숫자를 내림해 정수를 반환함.
+
+// console.log(Math.floor(3.1415926535))
+
+
+// Math.max()
+// 주어진 숫자 중 가장 큰 숫자를 반환함.
+
+// console.log(Math.max(1, 22, 38, 1000, 192))
+
+
+
+// Math.min()
+// 주어진 숫자 중 가장 작은 숫자를 반환함.
+
+// console.log(Math.min(1, 22 ,38, 192, -10))
+
+
+
+// Math.pow()
+// 주어진 숫자의 거듭제곱한 값을 반환함.
+
+// console.log(Math.pow(4,2))
+// console.log(Math.pow(7,2))
+// console.log(Math.pow(10,3))
+
+
+// Math.random()
+// 숫자 0 이상, 1미만의 난수를 반환함.
+
+// console.log(Math.random())
+
+// // 특정 범위의 랜덤 함수를 얻는 함수
+// function random(min = 0, max = 10) {
+//   return Math.floor(Math.random() * (max - min)) + min
+// }
+
+// console.log(random())
+// console.log(random(11,20))
+// console.log(random(101,999))
+
+
+// Math.round()
+// 주어진 숫자를 반올림해 정수를 반환함.
+
+const num1 = 3.141
+const num2 = 3.768
+
+console.log(Math.round(num1))
+console.log(Math.round(num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
