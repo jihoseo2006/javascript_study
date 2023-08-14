627a66a43da3c35dda05fec8750a615a1ab2c8c4
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-08-14 14:27:50 +0900

    DOM1

--- javascript/main.js
+++ javascript/main.js
@@ -2067,4 +2067,40 @@
 // }
 // wrap()
 
+//DOM(Document Object Model)
+//DOM이란 HTML 문서를 객체로 표현한 것으로,
+//JS에서 HTML을 제어할 수 있게 해준다.
 
+// const element = document.querySelector('h1')
+// console.log(element.textContent)
+
+
+
+//DOM API
+
+//NODE VS Element
+
+//노드(Node): HTML 요소, 텍스트, 주석 등 모든 것을 의미
+//요소(Element): HTML 요소를 의미
+
+// const parent = document.querySelector('.parent')
+
+// //부모 요소의 모든 자식 노드 확인!
+// console.log(parent.childNodes)
+
+// //부모 요소의 모든 자식 요소 확인!
+// console.log(parent.children)
+
+// console.dir(parent)
+
+
+class N {}
+class E extends N {}
+
+console.dir(E)
+console.dir(N)
+console.dir(E.__proto__) // (상속받은)상위 클래스 접속(일반 코드 작성시 사용은 권장안함)
+
+console.dir(Element)
+console.dir(Node)
+console.dir(Element.__proto__)
