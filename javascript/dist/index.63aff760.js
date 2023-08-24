95b1318579b41dfa240708a6fe29dbb258ddf8ca
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-08-24 23:34:31 +0900

    console 이벤트(1)
    서식 문자 치환 들어가다가 끝남

--- javascript/dist/index.63aff760.js
+++ javascript/dist/index.63aff760.js
@@ -2392,10 +2392,62 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 // -warn: 경고 메시지
 // -error: 에러 메시지
 // -dir: 속성을 볼 수 있는 객체를 출력
-console.log(document.body);
-console.warn(document.body);
-console.error(document.body);
-console.dir(document.body);
+// console.log(document.body)
+// console.warn(document.body)
+// console.error(document.body)
+// console.dir(document.body)
+// .count() .countReset()
+// 콘솔에 메소드 호출의 누적 횟수를 출력하거나 초기화한다.
+// console.count('이름')
+// console.countReset('이름')
+// console.count() //디폴트 값으로 카운트 됨
+// console.count('a')
+// console.count('a')
+// console.count('b')
+// console.count('a')
+// console.log('Reset a!')
+// console.countReset('a')
+// console.count('a')
+// console.count('b')
+// .time(), .timeEnd()
+// 콘솔에 타이머가 시작해서 종료되기까지의 시간(ms)을 출력한다.
+// console.time('이름')
+// console.timeEnd('이름')
+// console.time('반복문')
+// for (let i = 0; i < 10000; i+=1) {
+//   console.log(i)
+// }
+// console.timeEnd('반복문')
+// .trace()
+// 메소드 호출 스택(Call Stack)을 추적해 출력한다.
+// function a() {
+//   function b() {
+//     function c() {
+//       console.log('Log')
+//       console.trace()
+//     }
+//     c()
+//   }
+//   b()
+// }
+// a()
+// .clear()
+//  콘솔에 기록된 메시지를 모두 삭제한다.
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.clear()
+// 서식 문자 치환
+// %s - 문자로 적용
+// %o - 객체로 적용
+// %c - css를 적용
+const a = "The brown fox";
+const b = 3;
+const c = {
+    f: "fox",
+    d: "dog"
+};
+console.log();
 
 },{}]},["lmKb7","adjPd"], "adjPd", "parcelRequirebaba")
 
