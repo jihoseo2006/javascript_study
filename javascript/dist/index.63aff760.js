83e17b190199129d6a60abac5d35807f74cc8a93
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-08-27 00:28:59 +0900

    location

--- javascript/dist/index.63aff760.js
+++ javascript/dist/index.63aff760.js
@@ -2472,6 +2472,19 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 //   return cookie
 // }
 // console.log(getCookie('a'))
+// Location
+//  현제 페이지 정보를 반환하거나 제어한다.
+// .href: 전체 URL 주소
+// .protocol: 프로토콜
+// .hostname: 도메인 이름
+// .pathname: 도메인 이후 경로
+// .host: 포트 번호를 포함한 도메인 이름
+// .port: 포트 번호
+// .hash: 해시 정보 (페이지의 ID)
+// .assign(주소): 해당 '주소'로 페이지 이동
+// .replace(주소): 해당 '주소'로 페이지 이동, 현재 페이지 히스토리를 제거
+// .reload(강력): 페이지 새로고침, `true` 인수는 '강력' 새로고침
+console.log(location);
 
 },{}]},["lmKb7","adjPd"], "adjPd", "parcelRequirebaba")
 
