ef084d17fab5cba040ddd9be4d57d245ef457a78
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2023-12-09 00:15:56 +0900

    클로저

--- javascript/main.js
+++ javascript/main.js
@@ -2695,12 +2695,31 @@
 // 데이터가 할당된 메모리에서 더 이상 사용되지 않는 데이터를 해체하는 것을 말한다
 // 가비지 컬랙션은 개발자가 직접 실행하거나 관리할 수 없다.
 
-const user ={
-  name: 'jiho',
-  age: '18',
-  emails: ['jihoseo2006@gmail.com', 'jihoseo2006@dsm.hs.kr']
+// const user ={
+//   name: 'jiho',
+//   age: '18',
+//   emails: ['jihoseo2006@gmail.com', 'jihoseo2006@dsm.hs.kr']
+// }
+
+// delete user.emails
+
+// console.log(user)
+
+// 클로저(Closure)
+
+// 함수가 선언 될 때의 유효범위 (렉시컬 범위)를 기억하고 있다가,
+// 함수가 외부에서 호출 될 때 그 유효범위의 특정 변수를 참조할 수 있는 개념을 말한다.
+
+function createCount() {
+  let a = 0
+  return function() {
+    return a += 1
+  }
 }
 
-delete user.emails
+const count = createCount()
 
-console.log(user)
+console.log(count()) //1
+console.log(count()) //2
+console.log(count()) //3
+ 
